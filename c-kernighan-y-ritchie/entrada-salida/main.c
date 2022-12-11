5712641a0bd2dbcfa466611beb0959eb0e210a53
Alvaro Valera <alvarovaleralopez@gmail.com>
2022-12-11 12:58:16 +0100

    Contando palabras.

--- c-kernighan-y-ritchie/entrada-salida/main.c
+++ c-kernighan-y-ritchie/entrada-salida/main.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
 
+#define IN 1
+#define OUT 0
+
 int main(){
 
-    int c, newLines;
+    int c, characters, words, lines, state;
 
-    newLines = 0;
+    characters = -1;
+    words = lines = 0;
+    state = OUT;
 
     do{
         c = getchar();
+        characters++;
         if(c == '\n')
-            newLines++;
+            lines++;
+        
+        if(c==' ' || c=='\t' || c=='\n' )
+            state = OUT;
+        else if(!state){
+            words++;
+            state = IN;
+        }
+
     }while(c!=EOF);
 
-    printf("%ld", newLines);
+    printf("There are %d lines.\nThere are %d words.\nThere are %d characters.", lines, words, characters);
 
     return 0;
 
