53f82b7ec5da10febca9159722e8098527a8d065
Alvaro Valera <alvarovaleralopez@gmail.com>
2022-12-11 13:13:17 +0100

    Contando palabras con modularizacion y punteros.

--- c-kernighan-y-ritchie/entrada-salida/main.c
+++ c-kernighan-y-ritchie/entrada-salida/main.c
@@ -1,28 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define IN 1
 #define OUT 0
+
+void checkCharacter(const char c, int* characters, int* words, int* lines, int* state){
+
+    ++*characters;
+    if(c == '\n')
+        ++*lines;
+    
+    if(c==' ' || c=='\t' || c=='\n')
+        *state = OUT;
+    else if(!*state){
+        ++*words;
+        *state = IN;
+    }
+
+}
 
 int main(){
 
     int c, characters, words, lines, state;
 
-    characters = -1;
-    words = lines = 0;
+    characters = words = lines = 0;
     state = OUT;
 
     do{
         c = getchar();
-        characters++;
-        if(c == '\n')
-            lines++;
-        
-        if(c==' ' || c=='\t' || c=='\n' )
-            state = OUT;
-        else if(!state){
-            words++;
-            state = IN;
-        }
+        checkCharacter(c, &characters, &words, &lines, &state);
 
     }while(c!=EOF);
 
