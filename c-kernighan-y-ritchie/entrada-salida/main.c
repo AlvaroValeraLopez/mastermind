6a20beb2816b53c8a8793f0fc79d41acf8da94e4
Alvaro Valera <alvarovaleralopez@gmail.com>
2022-12-11 12:31:33 +0100

    Contando los caracteres que tiene la entrada.

--- c-kernighan-y-ritchie/entrada-salida/main.c
+++ c-kernighan-y-ritchie/entrada-salida/main.c
@@ -2,12 +2,15 @@
 
 int main(){
 
-    int c;
+    long charactersNumber;
 
-    do{
-        c = getchar();
-        putchar(c);
-    }while(c!=EOF);
+    charactersNumber = 0;
+
+    while(getchar() != EOF)
+        charactersNumber++;
+
+    /* Esta función tambien cuenta el /n como caracter. */
+    printf("%ld", charactersNumber);
 
     return 0;
 
